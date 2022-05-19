887dceb38ef14b00180d896ad206709aaa9b8bb5
Kevin Meng <kevin.meng.01@gmail.com>
2022-05-19 00:36:18 -0400

    Fix MEND bug with mismatched matrix-lr relationships

--- baselines/mend/mend_main.py
+++ baselines/mend/mend_main.py
@@ -111,6 +111,7 @@ class MendRewriteExecutor:
         tokens = torch.tensor(self.tokenizer(sentence)["input_ids"])[None]
         label_tokens = tokens.clone()
         label_tokens[0][: -len(target_tokens)] = -100
+
         edit_inner = dict(
             input_ids=tokens.clone().cuda(),
             attention_mask=torch.ones_like(tokens).cuda(),
@@ -121,34 +122,26 @@ class MendRewriteExecutor:
             attention_mask=torch.ones_like(tokens).cuda(),
         )
         edited_model, model_info = self.alg.edit(edit_inner, cond, return_factors=True)
-        factors = {
-            k + "." + n: v.detach().cpu().numpy()
-            for k, pair in model_info["factors"].items()
-            for n, v in zip("uv", pair)
+
+        mean_grads = {
+            n: torch.einsum(f"bi,bj->ij", x, delta)
+            for n, (x, delta) in model_info["factors"].items()
         }
-        # Also keep these learned LRs.
-        factors["edit_lrs"] = self.alg.edit_lrs.detach().cpu().numpy()
+        edit_lrs = self.alg.edit_lrs.detach().clone()
 
-        d = factors
-        torch_factors = {k: torch.tensor(v) for k, v in d.items()}
-        eli = 0
-        edit_lrs = torch_factors["edit_lrs"]
         model = deepcopy(self.model) if copy else self.model
-
         w_backups = {}
 
         with torch.no_grad():
-            for n, p in model.named_parameters():
-                uname, vname = f"{n}.u", f"{n}.v"
-                if uname in torch_factors:
-                    if return_orig_weights:
-                        w_backups[n] = p.detach().clone()
+            for lr, (n, g) in zip(edit_lrs, mean_grads.items()):
+                cur_weight = nethook.get_parameter(model, n)
+                if return_orig_weights and n not in w_backups:
+                    w_backups[n] = cur_weight.detach().clone()
 
-                    if "gpt2" in hparams.model_name:
-                        delta = torch_factors[uname].t() @ torch_factors[vname]
-                    else:  # gpt-j, I'm guessing
-                        delta = torch_factors[vname].t() @ torch_factors[uname]
-                    p.add_((delta * edit_lrs[eli] * hparams.lr_scale).to(p.device))
-                    eli += 1
+                upd_matrix = lr * g * hparams.lr_scale
+                if upd_matrix.shape != cur_weight.shape:
+                    upd_matrix = upd_matrix.T
+
+                cur_weight[...] += upd_matrix
 
         return model, w_backups
