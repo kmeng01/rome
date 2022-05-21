7be0c071d3de218e9d6afccd03356e6a366035c7
Kevin Meng <kevin.meng.01@gmail.com>
2022-05-21 18:56:03 -0400

    Add new downloading actions

--- dsets/py/counterfact.py
+++ dsets/py/counterfact.py
@@ -1,14 +1,26 @@
 import typing
 from torch.utils.data import Dataset
+import torch
 from pathlib import Path
 import json
+
+from util.globals import *
+
+REMOTE_URL = f"{REMOTE_ROOT_URL}/data/dsets/counterfact.json"
 
 
 class CounterFactDataset(Dataset):
     def __init__(
-        self, data_dir: str, size: typing.Optional[int] = 10000, *args, **kwargs
+        self, data_dir: str, size: typing.Optional[int] = None, *args, **kwargs
     ):
-        with open(Path(data_dir) / "counterfact.json", "r") as f:
+        data_dir = Path(data_dir)
+        cf_loc = data_dir / "counterfact.json"
+        if not cf_loc.exists():
+            print(f"{cf_loc} does not exist. Downloading from {REMOTE_URL}")
+            data_dir.mkdir(exist_ok=True, parents=True)
+            torch.hub.download_url_to_file(REMOTE_URL, cf_loc)
+
+        with open(cf_loc, "r") as f:
             self.data = json.load(f)
         if size is not None:
             self.data = self.data[:size]
---
+++ dsets/py/knowns.py
@@ -0,0 +1,30 @@
+import typing
+from torch.utils.data import Dataset
+import torch
+from pathlib import Path
+import json
+
+from util.globals import *
+
+REMOTE_URL = f"{REMOTE_ROOT_URL}/data/dsets/known_1000.json"
+
+
+class KnownsDataset(Dataset):
+    def __init__(self, data_dir: str, *args, **kwargs):
+        data_dir = Path(data_dir)
+        known_loc = data_dir / "known_1000.json"
+        if not known_loc.exists():
+            print(f"{known_loc} does not exist. Downloading from {REMOTE_URL}")
+            data_dir.mkdir(exist_ok=True, parents=True)
+            torch.hub.download_url_to_file(REMOTE_URL, known_loc)
+
+        with open(known_loc, "r") as f:
+            self.data = json.load(f)
+
+        print(f"Loaded dataset with {len(self)} elements")
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, item):
+        return self.data[item]
