26b0841d2945a2f20e6d08daf28175932962c58d
Kevin Meng <kevin.meng.01@gmail.com>
2022-05-21 18:41:02 -0400

    Update globals to include remote

--- util/globals.py
+++ util/globals.py
@@ -4,7 +4,7 @@ from pathlib import Path
 with open("globals.yml", "r") as stream:
     data = yaml.safe_load(stream)
 
-RESULTS_DIR, DATA_DIR, STATS_DIR, COMPILED_DATASETS_DIR, TFIDF_DIR, HPARAMS_DIR = (
+RESULTS_DIR, DATA_DIR, STATS_DIR, COMPILED_DATASETS_DIR, TFIDF_DIR, HPARAMS_DIR, REMOTE_ROOT_URL = (
     Path(z)
     for z in [
         data["RESULTS_DIR"],
@@ -13,5 +13,6 @@ RESULTS_DIR, DATA_DIR, STATS_DIR, COMPILED_DATASETS_DIR, TFIDF_DIR, HPARAMS_DIR 
         data["COMPILED_DATASETS_DIR"],
         data["TFIDF_DIR"],
         data["HPARAMS_DIR"],
+        data["REMOTE_ROOT_URL"]
     ]
 )
