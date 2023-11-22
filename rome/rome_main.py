507dfd508e492de9a8286b34d62a7f104b94aace
Xuhan Zhao <sherryzhao00@gmail.com>
2023-11-22 15:00:24 -0500

    Update rome_main.py
    Change from updating a hard-coded layer to updating the layer with highest score

--- rome/rome_main.py
+++ rome/rome_main.py
@@ -37,7 +37,7 @@ def apply_rome_to_model(
     weights_copy = {}
 
     for i, request in enumerate(requests):
-        deltas = execute_rome(model, tok, request, hparams)
+        deltas = execute_rome_variation(model, tok, request, hparams)
 
         with torch.no_grad():
             for w_name, (delta_u, delta_v) in deltas.items():
@@ -76,6 +76,88 @@ def execute_rome(
         f"Executing ROME algorithm for the update: "
         f"[{request['prompt'].format(request['subject'])}] -> [{request['target_new']['str']}]"
     )
+
+    # Retrieve weights that user desires to change
+    weights = {
+        f"{hparams.rewrite_module_tmp.format(layer)}.weight": nethook.get_parameter(
+            model, f"{hparams.rewrite_module_tmp.format(layer)}.weight"
+        )
+        for layer in hparams.layers
+    }
+    # Save old weights for future restoration
+    weights_copy = {k: v.detach().clone() for k, v in weights.items()}
+
+    # Update loop: sequentially intervene at each specified layer
+    deltas = {}
+    for layer in sorted(hparams.layers):
+        # Compute rank-1 update matrix
+        left_vector: torch.Tensor = compute_u(
+            model,
+            tok,
+            request,
+            hparams,
+            layer,
+            get_context_templates(model, tok, hparams.context_template_length_params),
+        )
+        print("Left vector shape:", left_vector.shape)
+        right_vector: torch.Tensor = compute_v(
+            model,
+            tok,
+            request,
+            hparams,
+            layer,
+            left_vector,
+            get_context_templates(model, tok, hparams.context_template_length_params),
+        )
+        print("Right vector shape:", right_vector.shape)
+
+        with torch.no_grad():
+            # Determine correct transposition of delta matrix
+            weight_name = f"{hparams.rewrite_module_tmp.format(layer)}.weight"
+            upd_matrix = left_vector.unsqueeze(1) @ right_vector.unsqueeze(0)
+            upd_matrix = upd_matrix_match_shape(upd_matrix, weights[weight_name].shape)
+
+            # Update model weights and record desired changes in `delta` variable
+            weights[weight_name][...] += upd_matrix
+            deltas[weight_name] = (
+                left_vector.detach(),
+                right_vector.detach(),
+            )
+
+    # Restore state of original model
+    with torch.no_grad():
+        for k, v in weights.items():
+            v[...] = weights_copy[k]
+
+    print(f"Deltas successfully computed for {list(weights.keys())}")
+
+    return deltas
+
+# rome variation, edit at max-score-layer only
+def execute_rome_variation(
+    model: AutoModelForCausalLM,
+    tok: AutoTokenizer,
+    request: Dict,
+    hparams: ROMEHyperParams,
+) -> Dict[str, Tuple[torch.Tensor]]:
+    """
+    Executes the ROME update algorithm for the specified update at the specified layer
+    Invariant: model at beginning of function == model at end of function
+    """
+
+    # Update target and print info
+    request = deepcopy(request)
+    if request["target_new"]["str"][0] != " ":
+        # Space required for correct tokenization
+        request["target_new"]["str"] = " " + request["target_new"]["str"]
+    print(
+        f"Executing ROME variation algorithm for the update: "
+        f"[{request['prompt'].format(request['subject'])}] -> [{request['target_new']['str']}]"
+    )
+
+    scores = torch.Tensor(request["scores"])
+    end_idx = request["subject_range"][1]
+    layers = [torch.argmax(scores[end_idx])]
 
     # Retrieve weights that user desires to change
     weights = {
